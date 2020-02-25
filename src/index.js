5a3af44f654fbfc992cc3042f87bf08772155308
Quazaru <qilaur@mail.ru>
2020-02-25 21:16:20 +0300

    init:start work

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || !array.length) {
+        return 0
+    } else {
+        return Math.min(...array);
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || !array.length) {
+        return 0
+    } else {
+        return Math.max(...array);
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || !array.length) {
+        return 0
+    } else {
+        const average = array.reduce((a, b) => (a + b)) / array.length;
+        return average;
+    }
 }
